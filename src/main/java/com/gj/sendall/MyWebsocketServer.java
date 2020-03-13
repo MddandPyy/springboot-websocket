b9641e2584e4d17329824a689c061774d9a043b4
liuzhuopeng <lzhuopeng@163.com>
2020-03-13 16:48:11 +0800

    优化

--- src/main/java/com/gj/sendall/MyWebsocketServer.java
+++ src/main/java/com/gj/sendall/MyWebsocketServer.java
@@ -1,4 +1,4 @@
-package com.gj.sendall;
+package com.gj.sendone;
 
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
@@ -6,64 +6,86 @@ import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * 群发第一版
+ * 群发最终版
  * @param
  * @return
  * @date 2020/3/13
  * @ahthor lzp
  */
-@ServerEndpoint("/test")
+@ServerEndpoint("/testroom")
 @Component
 @Slf4j
 public class MyWebsocketServer {
     /**
-     * 存放所有在线的客户端
+     * 用于存放所有在线客户端
      */
     private static Map<String, Session> clients = new ConcurrentHashMap<>();
+
     private Gson gson = new Gson();
 
     @OnOpen
     public void onOpen(Session session) {
-        log.info("有新的客户端连接了: {}", session.getId());
-        //将新用户存入在线的组
+        Map<String, List<String>>  requestParameterMap = session.getRequestParameterMap();
+        List<String> list = requestParameterMap.get("username");
+        String username = list.get(0); // 当前用户名*/
+
+        log.info("有新的客户端上线: {}", session.getId());
         clients.put(session.getId(), session);
+        sendAll(username+"上线了");
+
     }
 
-    /**
-     * 客户端关闭
-     * @param session session
-     */
     @OnClose
     public void onClose(Session session) {
-        log.info("有用户断开了, id为:{}", session.getId());
-        //将掉线的用户移除在线的组里
-        clients.remove(session.getId());
+        String sessionId = session.getId();
+        log.info("有客户端离线: {}", sessionId);
+        clients.remove(sessionId);
+        Map<String, List<String>>  requestParameterMap = session.getRequestParameterMap();
+        List<String> list = requestParameterMap.get("username");
+        String username = list.get(0); // 当前用户名*/
+        sendAll(username+":下线了");
     }
 
-    /**
-     * 发生错误
-     * @param throwable e
-     */
     @OnError
-    public void onError(Throwable throwable) {
+    public void onError(Session session, Throwable throwable) {
+        String sessionId = session.getId();
+        if (clients.get(sessionId) != null) {
+            log.info("发生了错误,移除客户端: {}", sessionId);
+            clients.remove(sessionId);
+        }
         throwable.printStackTrace();
     }
 
-
-    /**
-     * 收到客户端发来消息
-     * @param message  消息对象
-     */
     @OnMessage
-    public void onMessage(String message) {
-        log.info("服务端收到客户端发来的消息: {}", message);
-        this.sendAll(message);
+    public void onMessage(String message,Session session) {
+        log.info("收到客户端发来的消息: {}", message);
+        Map<String, List<String>>  requestParameterMap = session.getRequestParameterMap();
+        List<String> list = requestParameterMap.get("username");
+        String username = list.get(0); // 当前用户名*/
+        sendAll(username+":"+message);
     }
 
+    /**
+     * 发送消息
+     *
+     * @param message 消息对象
+     */
+    private void sendTo(String message,Session session) {
+        Session s = clients.get(session.getId());
+        if (s != null) {
+            try {
+                s.getBasicRemote().sendText("服务端单对单消息"+message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     /**
      * 群发消息
@@ -71,8 +93,7 @@ public class MyWebsocketServer {
      */
     private void sendAll(String message) {
         for (Map.Entry<String, Session> sessionEntry : clients.entrySet()) {
-            sessionEntry.getValue().getAsyncRemote().sendText("服务端群发消息"+message);
+            sessionEntry.getValue().getAsyncRemote().sendText(message);
         }
     }
-
 }
